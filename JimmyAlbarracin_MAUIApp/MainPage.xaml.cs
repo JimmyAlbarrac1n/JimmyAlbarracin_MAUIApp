436d1e2e4776763837adf70bbbea302d89c26488
JimmyAlbarrac1n <jimmismael2003@gmail.com>
2024-05-22 11:42:27 -0500

    code behind

--- JimmyAlbarracin_MAUIApp/MainPage.xaml.cs
+++ JimmyAlbarracin_MAUIApp/MainPage.xaml.cs
@@ -1,4 +1,7 @@
-﻿namespace JimmyAlbarracin_MAUIApp
+﻿using JimmyAlbarracin_MAUIApp.Models;
+using Newtonsoft.Json;
+
+namespace JimmyAlbarracin_MAUIApp
 {
     public partial class MainPage : ContentPage
     {
@@ -10,8 +13,17 @@
 
         private void Button_Clicked(object sender, EventArgs e)
         {
-
+            var client = new HttpClient();
+            client.BaseAddress = new Uri("https://localhost:7255/api/");
+            var response = client.GetAsync("burger").Result;
+            if (response.IsSuccessStatusCode)
+            {
+                var burgers = response.Content.ReadAsStringAsync().Result;
+                var burgersList =
+               JsonConvert.DeserializeObject<List<Burger>>(burgers);
+                listView.ItemsSource = burgersList;
+            }
         }
-    }
 
+    }
 }
